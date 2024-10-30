83de0d3d2242ba05a1f867c4448653c758aa817b
selinoi123 <selinoi123@gmail.com>
2024-10-30 21:37:19 +0200

    Add files via upload

---
+++ question_number_1.py
@@ -0,0 +1,9 @@
+#START
+
+x: int = int(input("Enter a number : "))
+
+print(x)
+print(x)
+print(x)
+
+#END
---
+++ question_number_2.py
@@ -0,0 +1,10 @@
+#START
+
+x: int = 777
+y: int = 888
+z: int = 999
+
+result: int = x * y + z
+print(result)
+
+#END
